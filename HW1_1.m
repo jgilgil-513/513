17900842f02d407764d1ca62da3c953c1a8e50e2
jgilgil-513 <70353821+jgilgil-513@users.noreply.github.com>
2020-09-01 18:50:34 -0400

    Add files via upload

---
+++ HW1_1.m
@@ -0,0 +1,37 @@
+clc,close all,clear all 
+
+dy=0.001; % increment in y
+y1(1)=0;
+N = 51; %use odd number so lines align
+d = 5; %charge distance
+q = 1E-5;
+th = linspace(0,2*pi(),N);
+steps = 20000;
+
+%Plot positive paricle E-field lines
+for j = 1:N
+
+    [x,y] = ode_gilgilP(d,q,th(j),steps);
+    
+    X(:,j) = x;
+    Y(:,j) = y;
+end
+
+figure(1)
+plot(X,Y);
+hold on;
+
+%Plot negative particle E-field lines like if it was a positive charge
+for j = 1:N
+
+    [x,y] = ode_gilgilN(d,q,th(j),steps);
+   
+    X(:,j) = x;
+    Y(:,j) = y;
+end
+
+plot(X,Y);
+axis([-N N -N N])
+xlabel('x')
+ylabel('y')
+title('Homework1.1','Interpreter','Latex');
---
+++ ode_gilgilN.m
@@ -0,0 +1,41 @@
+    
+function [x,y]= ode_gilgilN(d,q,th,steps)    
+
+    dt = 0.01;
+    
+    to = 0;
+    
+    qp = q;
+    qn =-q; 
+    
+   
+   
+    x(1) = -d+cos(th); 
+    
+    y(1) = 0+sin(th);
+    %ignore Z component
+    
+
+    
+     eo = 8.85E-12;
+     k = 1/(4*pi()*eo);
+      
+      
+
+    %fprintf('t \t x \t y \n')
+    for i = 1:steps
+        %fprintf('%.2f\t%.2f\t%.2f\n',to(i),x(i),y(i));
+       
+        Ex = k*qp*(x(i)+d)/((x(i)+d)^2+y(i)^2)^(3/2)+k*qn*(x(i)-d)/((x(i)-d)^2+y(i)^2)^(3/2);
+        Ey = k*qp*y(i)/((x(i)+d)^2+y(i)^2)^(3/2)+k*qn*y(i)/((x(i)-d)^2+y(i)^2)^(3/2);
+        
+        
+        E = sqrt(Ex^2+Ey^2);
+        
+        x(i+1) = x(i) + Ex/E*dt;
+        y(i+1) = y(i) + Ey/E*dt;
+        
+        to(i+1) = to(i) + dt;
+    end
+
+   
